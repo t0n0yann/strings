1b7257f2bba38b3dc8a22a1a142392eb1a91472c
t0n0yann <89076837+t0n0yann@users.noreply.github.com>
2021-11-03 16:50:23 +0300

    strings

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string y;
+    cin >> y;
+    string x = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+    cout << x.find(y) << "\n";
+    int b = x.find(y);
+    string a = y;
+    int j = a.length();
+    cout << b + (j - 1);
+}
+
